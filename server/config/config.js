e25c49b3f1beed5f22be7bb58d26e50ac0f18354
Javier Mendez <jmendezmch@gmail.com>
2018-09-10 21:13:21 -0300

    fix config file

--- server/config/config.js
+++ server/config/config.js
@@ -4,10 +4,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // BASE DE DATOS
 let urlDB;
-// if (process.env.NODE_ENV == 'dev') {
-//     urlDB = 'mongodb://localhost:27017/jazo-cafe';
-// } else {
-urlDB = 'mongodb://jazo:jujuy969@ds033828.mlab.com:33828/jazo-cafe'
-    // }
+if (process.env.NODE_ENV == 'dev') {
+    urlDB = 'mongodb://localhost:27017/jazo-cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
